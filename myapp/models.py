45c0c7ced20900c3e48f5db9245b27f724e8e73c
Jane <113984107+ardojane@users.noreply.github.com>
2025-05-22 22:11:50 +0800

    fixed?
    Updated version. Aafter bantayan

--- myapp/models.py
+++ myapp/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.png')
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
 
 
 class Task(models.Model):
